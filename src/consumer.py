2c8563ced511f1286f2c78ce25e555f59694d4bf
Edward Zhou <edz@Edz.local>
2022-04-26 20:09:08 -0400

    kafka consume fake data

--- src/consumer.py
+++ src/consumer.py
@@ -1,4 +1,15 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('test_topic')
-for msg in consumer:
-    print(msg)
+import json
+import time
+
+if __name__ == '__main__':
+    consumer = KafkaConsumer('registered_user',
+                             bootstrap_servers='localhost:9092',
+                             auto_offset_reset='earliest',
+                             group_id='consumer-group-A')
+    print('starting consumer...')
+    time.sleep(3)
+    for msg in consumer:
+        print(json.loads(msg.value))
+        # time.sleep(1)
+ConsumerRecord(topic='registered_user', partition=0, offset=15, timestamp=1651018002643, timestamp_type=0, key=None, value=b'{"name": "Amber Robbins", "address": "180 Morgan Common\\nSonyashire, TN 43350", "year": "1973"}', headers=[], checksum=None, serialized_key_size=-1, serialized_value_size=95, serialized_header_size=-1)
--- src/producer.py
+++ src/producer.py
@@ -1,5 +1,25 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:1234')
-for _ in range(100):
-    producer.send('foobar', b'some_msg_bytes')
+import time
+import json
+from data import get_registered_user
 
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
+def get_partition(key, all, available):
+    return 0
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092'
+                         , value_serializer=json_serializer)
+# ,partitioner=get_partition)
+
+if __name__ == '__main__':
+    while True:
+        registered_user = get_registered_user()
+        print(registered_user)
+        # send(topic_name, data)
+        producer.send('registered_user', registered_user)
+        time.sleep(2)
